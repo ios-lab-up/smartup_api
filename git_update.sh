89cd65fd0e07f42c30f4f4c3786858a736fd78e4
luisced <luis.cedillo.maldonado@gmail.com>
2023-04-23 00:54:13 -0600

    fixed tagging

--- git_update.sh
+++ git_update.sh
@@ -2,14 +2,13 @@
 
 VERSION=""
 
-#get parameters
+# get parameters
 while getopts v: flag
 do
-    case "${flag}" in
-        v) VERSION=${OPTARG};;
-    esac
+  case "${flag}" in
+    v) VERSION=${OPTARG};;
+  esac
 done
-
 
 # get highest tag number, and add v0.1.0 if doesn't exist
 git fetch --prune --unshallow 2>/dev/null
@@ -17,47 +16,50 @@ CURRENT_VERSION=`git describe --abbrev=0 --tags 2>/dev/null`
 
 if [[ $CURRENT_VERSION == '' ]]
 then
-    CURRENT_VERSION='v0.1.0'
+  CURRENT_VERSION='v0.1.0'
 fi
 echo "Current Version: $CURRENT_VERSION"
+
+# replace . with space so can split into an array
+CURRENT_VERSION_PARTS=(${CURRENT_VERSION//./ })
 
 # get number parts
 VNUM1=${CURRENT_VERSION_PARTS[0]}
 VNUM2=${CURRENT_VERSION_PARTS[1]}
 VNUM3=${CURRENT_VERSION_PARTS[2]}
 
-# increase version
-if [[ $VERSION == 'major'  ]]
+if [[ $VERSION == 'major' ]]
 then
-    VNUM1=$((VNUM1+1))
-elif [[ $VERSION == 'minor'  ]]
+  VNUM1=v$((VNUM1+1))
+elif [[ $VERSION == 'minor' ]]
 then
-    VNUM2=$((VNUM2+1))
-elif [[ $VERSION == 'patch'  ]]
+  VNUM2=$((VNUM2+1))
+elif [[ $VERSION == 'patch' ]]
 then
-    VNUM3=$((VNUM3+1))
+  VNUM3=$((VNUM3+1))
 else
-    echo "No version type (major, minor, patch) was specified"
-    exit 1
+  echo "No version type (https://semver.org/) or incorrect type specified, try: -v [major, minor, patch]"
+  exit 1
 fi
 
 # create new tag
 NEW_TAG="$VNUM1.$VNUM2.$VNUM3"
 echo "($VERSION) updating $CURRENT_VERSION to $NEW_TAG"
 
-# get current hash an see if it already has a tag
+# get current hash and see if it already has a tag
 GIT_COMMIT=`git rev-parse HEAD`
 NEEDS_TAG=`git describe --contains $GIT_COMMIT 2>/dev/null`
 
 # only tag if no tag already
 if [ -z "$NEEDS_TAG" ]; then
-    echo "Tagged with $NEW_TAG"
-    git tag $NEW_TAG
-    git push --tags
-    git push
+  echo "Tagged with $NEW_TAG"
+  git tag $NEW_TAG
+  git push --tags
+  git push
 else
-    echo "Already a tag on this commit"
+  echo "Already a tag on this commit"
 fi
 
 echo ::set-output name=git-tag::$NEW_TAG
+
 exit 0
