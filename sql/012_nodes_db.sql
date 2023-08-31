54ce1429e3d2ba7c17017cba5191a63b5d0eb299
Mayze2602 <juanestebanmayoral@gmail.com>
2023-08-30 22:57:17 -0600

    Create 012_nodes_db.sql

---
+++ sql/012_nodes_db.sql
@@ -0,0 +1,21 @@
+-- Adminer 4.8.1 MySQL 8.1.0 dump
+
+SET NAMES utf8;
+SET time_zone = '+00:00';
+SET foreign_key_checks = 0;
+SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
+
+SET NAMES utf8mb4;
+
+DROP TABLE IF EXISTS `Nodes`;
+CREATE TABLE `Nodes` (
+  `ID` int NOT NULL AUTO_INCREMENT,
+  `id_node` tinytext NOT NULL,
+  `neighbors` json NOT NULL,
+  `Extra` int NOT NULL,
+  `status` int NOT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+
+-- 2023-08-31 04:45:15
