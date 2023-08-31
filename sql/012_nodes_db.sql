8656a5663b52c4b5fafc95d6fa9c2a249eaefcf4
Esteban Mayoral <juanestebanmayoral@gmail.com>
2023-08-31 13:34:01 -0600

    Update 012_nodes_db.sql
    updated nodes database

--- sql/012_nodes_db.sql
+++ sql/012_nodes_db.sql
@@ -10,12 +10,14 @@ SET NAMES utf8mb4;
 DROP TABLE IF EXISTS `Nodes`;
 CREATE TABLE `Nodes` (
   `ID` int NOT NULL AUTO_INCREMENT,
-  `id_node` tinytext NOT NULL,
+  `id_node` tinytext CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `id_name` text NOT NULL,
   `neighbors` json NOT NULL,
+  `id_floor` tinyint NOT NULL,
   `Extra` int NOT NULL,
   `status` int NOT NULL,
   PRIMARY KEY (`ID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 
--- 2023-08-31 04:45:15
+-- 2023-08-31 19:31:18
