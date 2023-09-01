48f942a4603d0cbd17dab2014289e44c548f8650
Xayil <xayil.gph@gmail.com>
2023-09-01 17:34:36 -0600

    Cambio al endpoint de nodes

--- App/school/nodes/routes.py
+++ App/school/nodes/routes.py
@@ -1,7 +1,52 @@
 from flask import Blueprint, request, jsonify
+import json
 
 nodes = Blueprint('nodes', __name__)
 
-@nodes.route('/find_path',method=['GET'])
+nodes = [
+    {
+        "ID": str,
+        "ID_Nodo": "AS0",
+        "ID_Nombre": "Entrada / Anexo",
+        "ID_Vecinos": [
+            {
+                "ID": str,
+                "ID_Nodo": "AP1",
+                "Codto": int,
+            }
+        ],
+        "Piso": 1,
+        "Status": bool
+    },
+    {
+        "ID": str,
+        "ID_Nodo": "AP1",
+        "ID_Nombre": "Pasillo",
+        "ID_Vecinos": [
+            {
+                "ID": str,
+                "ID_Nodo": "AS0",
+                "Costo": int,
+            },
+            {
+                "ID": str,
+                "ID_Nodo": "AS1",
+                "Costo": int,
+            },
+            {
+              "ID": str,
+              "ID_Nodo": "AP2",
+              "Costo": int,
+            }
+        ],
+        "Piso": 1,
+        "Status": bool
+    }
+]
+
+@nodes.route('/find/path',method=['GET'])
 def find_path():
-    pass
+    return jsonify({"nodes": nodes})
+
+if __name__ == '__main__':
+    nodes.run(debug=True)
