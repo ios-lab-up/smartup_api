e6cc8b739bdb242275059e1b8a6fdb89ceca5bd8
Mauricio <0253214@up.edu.mx>
2023-09-01 19:03:48 -0600

    deleted trash files under scripts, detected multiple misconfigurations under App/schools/nodes, changes sql nodes table structure

--- App/school/nodes/routes.py
+++ App/school/nodes/routes.py
@@ -1,9 +1,19 @@
 from flask import Blueprint, request, jsonify
 import json
 
-nodes = Blueprint('nodes', __name__)
 
-nodes = [
+# recuerda dar de alta la blueprint en el archivo App/school/__init__.py
+nodes = Blueprint('nodes', __name__)
+# el import esta comentado para que no cause un error al momento de ejecutar el ciclo main
+
+
+
+# No pueden haber variable globales en un archivo de rutas
+# nodes = []
+# esto es una variable global
+# tienen que estar en el archivo utils.py de su respectivo módulo
+
+nodes = [ # mueve esto a un archivo utils.py
     {
         "ID": str,
         "ID_Nodo": "AS0",
@@ -44,9 +54,29 @@ nodes = [
     }
 ]
 
-@nodes.route('/find/path',method=['GET'])
-def find_path():
-    return jsonify({"nodes": nodes})
 
+# El simbolo / solo se utiliza cuando hay multiples rutas en un mismo archivo
+# En este caso, el archivo nodes/routes.py solo tiene una ruta
+@nodes.route('/find/path',method=['GET']) # hay que añadir el metodo POST, ya que por defecto es GET
+# hay que renombrar la función a find_path, o algun parecido a lo que hace, como pathfinder
+def find_path():
+
+    # request.get_json() # esto es para obtener el json que se envia en el body de la petición
+    return jsonify({"nodes": nodes}) # se esta regresando un json, no un diccionario
+    # un endpoint siempre debe terminar con un return, ya sea un json, un string, un diccionario, etc.
+    # tu diccionario solo tiene una llave, nodes, y su valor es una lista de diccionarios
+    # por ende, es mejor regresar una lista de diccionarios, no un diccionario con una lista de diccionarios
+    # return jsonify(nodes)
+
+
+# borra lo que esta debajo de este comentario
+
+
+# Esta funcion va a causar un error, ya que no se puede ejecutar el ciclo main en un archivo de rutas
+# y estás borrando la blueprint al momento de declarar la variable nodes
 if __name__ == '__main__':
     nodes.run(debug=True)
+
+# El ciclo main nunca puede ser ejecutado en un archivo de rutas
+# El ciclo main solo puede ser ejecutado en el archivo principal de la aplicación
+# En este caso, el archivo principal de la aplicación es App/run.py
--- sql/012_nodes_db.sql
+++ sql/012_nodes_db.sql
@@ -1,5 +1,3 @@
--- Adminer 4.8.1 MySQL 8.1.0 dump
-
 SET NAMES utf8;
 SET time_zone = '+00:00';
 SET foreign_key_checks = 0;
@@ -10,14 +8,11 @@ SET NAMES utf8mb4;
 DROP TABLE IF EXISTS `Nodes`;
 CREATE TABLE `Nodes` (
   `ID` int NOT NULL AUTO_INCREMENT,
-  `id_node` tinytext CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `id_node` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   `id_name` text NOT NULL,
   `neighbors` json NOT NULL,
   `id_floor` tinyint NOT NULL,
-  `Extra` int NOT NULL,
-  `status` int NOT NULL,
+  `status` tinyint NOT NULL,
+  `Extra` text,
   PRIMARY KEY (`ID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
-
--- 2023-08-31 19:31:18
